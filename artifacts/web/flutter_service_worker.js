3f113cfa3b7eb41f458549b371826bff67950de0
AdamHendersonAquaQ <AdamHendersonAquaQ@users.noreply.github.com>
2023-05-25 10:56:16 +0000

    Commit the artifacts.

--- artifacts/web/flutter_service_worker.js
+++ artifacts/web/flutter_service_worker.js
@@ -28,15 +28,15 @@ const RESOURCES = {"flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "assets/FontManifest.json": "2f1ebedf6364e441385272a1fe631bb1",
 "assets/AssetManifest.json": "124c9a811dff11236efd6ff7d2f0d7f2",
 "assets/AssetManifest.smcbin": "1a27eca8163277c75b309616bad3faa8",
-"index.html": "2cc9deb4d35336efb0bb9d717c54ba3a",
-"/": "2cc9deb4d35336efb0bb9d717c54ba3a",
+"index.html": "d664eed5366b157d555b304647ebf075",
+"/": "d664eed5366b157d555b304647ebf075",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "cdcc388afc7a52f59c3cbb28e25bf848",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"main.dart.js": "efbe51769a764c8d70b29a25b059d2aa",
+"main.dart.js": "08d37b7a7f29a3317c0344d5183649bc",
 "manifest.json": "3687d41cac32628ae084b0ba29a75200"};
 // The application shell files that are downloaded before a service worker can
 // start.
