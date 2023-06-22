aadace3e27fd17c657951037b018da9807f5224e
AdamHendersonAquaQ <112879343+AdamHendersonAquaQ@users.noreply.github.com>
2023-06-22 11:12:05 +0100

    moved amplify config

--- lib/amplify/amplify_service.dart
+++ lib/amplify/amplify_service.dart
@@ -1,5 +1,5 @@
 import 'package:amplify_flutter/amplify_flutter.dart';
-import 'package:praxis_internals/amplify/amplifyconfiguration.dart';
+import 'package:praxis_internals/amplify/env_config.dart';
 import 'package:praxis_internals/authentication/authentication_service.dart';
 
 class AmplifyService {
@@ -16,3 +16,60 @@ class AmplifyService {
     }
   }
 }
+
+var amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "${EnvConfig.poolId1}",
+                            "Region": "${EnvConfig.region}"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "${EnvConfig.poolId2}",
+                        "AppClientId": "${EnvConfig.appClientId}",
+                        "Region": "${EnvConfig.region}"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "socialProviders": [],
+                        "usernameAttributes": [
+                            "EMAIL"
+                        ],
+                        "signupAttributes": [],
+                        "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": [
+                                "REQUIRES_LOWERCASE",
+                                "REQUIRES_NUMBERS",
+                                "REQUIRES_SYMBOLS",
+                                "REQUIRES_UPPERCASE"
+                            ]
+                        },
+                        "mfaConfiguration": "OFF",
+                        "mfaTypes": [
+                            "SMS"
+                        ],
+                        "verificationMechanisms": [
+                            "EMAIL"
+                        ]
+                    }
+                }
+            }
+        }
+    }
+}''';
